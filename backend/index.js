f4fcc2d8ea7a4a9df224e12b46f230540b3f8d4f
Shubhansh0311 <shubhanshagrawal0311@gmail.com>
2024-04-15 02:01:25 +0530

    url changed

--- backend/index.js
+++ backend/index.js
@@ -14,10 +14,9 @@ import connectDB from './db.js'
 const app = express()
 app.use(express.json())
 const corsOptions = {
-  origin: 'https://settingappbackend.vercel.app',
+  origin: 'https://settingappfrontend.vercel.app',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
-app.use(cors())
 app.use(cors(corsOptions));
 // app.use(cors())
 const port  = process.env.PORT || 8000;
