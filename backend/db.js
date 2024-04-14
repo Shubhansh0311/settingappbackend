dd303e738f6489b5a1511c8e4180ce1d42be90b2
Shubhansh0311 <shubhanshagrawal0311@gmail.com>
2024-04-15 01:19:27 +0530

    api change

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 const connectDB = async () => {
   try {
-    const db = await mongoose.connect("mongodb+srv://shubhansh:shubhansh@cluster0.ia7xyfr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/test")
+    const db = await mongoose.connect("mongodb+srv://shubhansh:shubhansh@cluster0.ia7xyfr.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0/test")
 
     console.log('database connected')
   } catch (err) {
